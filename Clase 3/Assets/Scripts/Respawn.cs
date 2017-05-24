36bdafa275995adb57c91514691214d971a06818
Aniluj <julianvolonterio@gmail.com>
2017-05-23 22:22:48 -0300

    Comienzo de spawn
    Creación de un script para respawnear en distintos puntos del mapa cuando se presiona una tecla (todavía incompleto).

---
+++ Clase 3/Assets/Scripts/Respawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Respawn : MonoBehaviour {
+
+	public Slider vida;
+	public GameObject[] puntosDeSpawn;
+	public KeyCode teclaParaRespawnear;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (vida.value == 10) {
+			if (Input.GetKeyDown (teclaParaRespawnear)) {
+				
+			}
+		}
+	}
+}
